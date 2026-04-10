ede0219d146b6781e2ea12c6ba9059a7de637af1
Ankit <ankitrajdihara123@gmail.com>
2026-04-10 13:23:44 +0530

    added_zod_schema_common_

--- excile_draw/apps/https-backend/src/index.ts
+++ excile_draw/apps/https-backend/src/index.ts
@@ -2,9 +2,21 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRETS } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {
+  CreateRoomSchema,
+  CreateSigninSchema,
+  CreateUserSchema,
+} from "@repo/common/types";
 const app = express();
 
 app.post("/signin", (req, res) => {
+  const data = CreateSigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect Input ",
+    });
+    return;
+  }
   const userId = 1;
   const token = jwt.sign(
     {
@@ -18,11 +30,29 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect Input ",
+    });
+    return;
+  }
   res.json({
     roomId: 123,
   });
 });
-app.post("/signup", (req, res) => {});
+app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect Input ",
+    });
+    return;
+  }
+  res.json({
+    userId: "123",
+  });
+});
 app.listen(4000, () => {
   console.log("listening to the port 4000");
 });
